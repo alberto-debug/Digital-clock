10ca63fec3b08d8a11bce81474539944cf5b232c
Alberto <albertojunior848423803@gmail.com>
2025-02-10 12:00:00 +0300

    Commit for February 10, 2025

--- relogio.lua/relogio.lua
+++ relogio.lua/relogio.lua
@@ -1,32 +1,32 @@
--- Função para limpar o terminal
+-- Função para limpar o termina
 function limpar_tela()
-  if os.getenv("OS") == "Windows_NT" then
-    os.execute("cls")
-  else
-    os.execute("clear")
-  end
+	if os.getenv("OS") == "Windows_NT" then
+		os.execute("cls")
+	else
+		os.execute("clear")
+	end
 end
 
 -- Função para exibir o relógio
 function relogio_minimalista()
-  while true do
-    limpar_tela()
+	while true do
+		limpar_tela()
 
-    -- Obter a hora atual
-    local hora_atual = os.date("*t")
-    local horas = string.format("%02d", hora_atual.hour)
-    local minutos = string.format("%02d", hora_atual.min)
-    local segundos = string.format("%02d", hora_atual.sec)
+		-- Obter a hora atual
+		local hora_atual = os.date("*t")
+		local horas = string.format("%02d", hora_atual.hour)
+		local minutos = string.format("%02d", hora_atual.min)
+		local segundos = string.format("%02d", hora_atual.sec)
 
-    -- Exibir o relógio estilizado
-    print(string.rep("=", 30))
-    print(string.format(" Relógio Minimalista "))
-    print("\n        " .. horas .. ":" .. minutos .. ":" .. segundos .. "        \n")
-    print(string.rep("=", 30))
+		-- Exibir o relógio estilizado
+		print(string.rep("=", 30))
+		print(string.format(" Relógio Minimalista "))
+		print("\n        " .. horas .. ":" .. minutos .. ":" .. segundos .. "        \n")
+		print(string.rep("=", 30))
 
-    -- Esperar 1 segundo antes de atualizar
-    os.execute("sleep 1") -- No Windows, pode ser necessário instalar o comando sleep
-  end
+		-- Esperar 1 segundo antes de atualizar
+		os.execute("sleep 1") -- No Windows, pode ser necessário instalar o comando sleep
+	end
 end
 
 -- Executar o relógio
